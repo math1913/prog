953bdce94f98768719c42068abec9adf63449a5e
math1913 <mateohuertas21@gmail.com>
2024-11-21 16:01:33 +0100

    subo minmax

---
+++ AEA2/minMax/minMax.java
@@ -0,0 +1,17 @@
+
+public class minMax {
+    public static void main(String[] args) {
+        int[] arr = {12, 3, 45, -3, 65, -5, 88, 99, 2, 23, 54, 33, 27, 53, 96, 0};
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min)
+                min = arr[i];
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+    }
+
+}
