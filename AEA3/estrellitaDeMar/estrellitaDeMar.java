9a8f3d0d607e28da68dcbe51e99d1ae4d6acfae9
Mateo <mateohuertas21@gmail.com>
2024-12-16 19:49:20 +0100

    Update estrellitaDeMar.java

--- AEA3/estrellitaDeMar/estrellitaDeMar.java
+++ AEA3/estrellitaDeMar/estrellitaDeMar.java
@@ -2,29 +2,16 @@ public class estrellitaDeMar {
     //Te queremos marina
     public static void main(String[] args) {
         estrellitaDeMar programa = new estrellitaDeMar();
-        programa.estrellas(7);
-        programa.guiones(1);
-        programa.numeros(5, 6);
-        programa.estrellas(3);
-        programa.guiones(5);
-        programa.numeros(3,2);
+        programa.imprimir("*", 7);
+        programa.imprimir("-",1);
+        programa.imprimir("5", 6);
+        programa.imprimir("*", 3);
+        programa.imprimir("-", 5);
+        programa.imprimir("3",2);
     }
-    public void estrellas(int cant){
-        for (int i = 0; i < cant; i++){
-            System.out.print("*");
-        }
-        System.out.println();
-    }
-    public void guiones(int cant){
-        for (int i = 0; i < cant; i++){
-            System.out.print("-");
-        }
-        System.out.println();
-    }
-    public void numeros(int num, int cant){
-        for (int i = 0; i < cant; i++){
-            System.out.print(num);
-        }
+    public void imprimir(String car, int cant) {
+        for (int i = 0; i < cant; i++)
+            System.out.print(car);
         System.out.println();
     }
 }
