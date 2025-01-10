7f9f3c2e6761dbee062151456fef53574402e034
Mateo <mateohuertas21@gmail.com>
2025-01-10 17:52:03 +0100

    Update cienNums.java

--- AEA2/cienNums/cienNums.java
+++ AEA2/cienNums/cienNums.java
@@ -1,15 +1,26 @@
 public class cienNums {
     public static void main(String[] args) {
-        int[] pares = new int[100];
-        int[] impares = new int[100];
+        int[][] pares = new int[10][10];
+        //int[] impares = new int[100];
+        //int[] suma = new int[100];
+        //int j = suma.length - 1;
+        int num = 0;
         for (int i = 0; i < pares.length; i++) {
-            pares[i] = i * 2;
-            System.out.print(pares[i] + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < impares.length; i++) {
-            impares[i] = i * 2 + 1;
-            System.out.print(impares[i] + " ");    
+            for (int j = 0; j < pares[i].length; j++) {
+                pares[i][j] = num * 2;
+                num++;
+                System.out.print(pares[i][j] + " ");
+            }
+            System.out.println();
+            
+
+
+            /*
+            impares[j] = j * 2 + 1;
+            suma[i] = pares[i] + impares[j];
+            System.out.print(suma[i] + " ");
+            j --;
+            */
         }
     }
 }
