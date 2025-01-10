9885b05c3fb5075e4dd759059777c46e777cefb3
Mateo <mateohuertas21@gmail.com>
2025-01-10 15:56:20 +0100

    impares tambien

--- AEA2/cienNums/cienNums.java
+++ AEA2/cienNums/cienNums.java
@@ -4,11 +4,11 @@ public class cienNums {
         int[] impares = new int[100];
         for (int i = 0; i < pares.length; i++) {
             pares[i] = i * 2;
-            impares[i] = i * 2 + 1;
             System.out.print(pares[i] + " ");
         }
         System.out.println();
         for (int i = 0; i < impares.length; i++) {
+            impares[i] = i * 2 + 1;
             System.out.print(impares[i] + " ");    
         }
     }
