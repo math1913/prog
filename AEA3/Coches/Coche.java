c123d5df5729ce63e392913145672f465cc6e04a
Mateo <mateohuertas21@gmail.com>
2025-03-05 15:30:50 +0100

    a

---
+++ AEA3/Coches/Coche.java
@@ -0,0 +1,13 @@
+package AEA3.Coches;
+public class Coche {
+    private int id;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+    public void tipoVehiculo(){
+    }
+}
